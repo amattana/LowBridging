b3a1b21d253e0748fccfaa12c2a9233380625ed8
Andrea Mattana <a.mattana80@gmail.com>
2019-02-26 14:14:43 +0100

    Manage only band 0-400

--- daily-process.py
+++ daily-process.py
@@ -31,9 +31,9 @@ if options.now:
             data = yesterday.strftime("%Y-%m-%d")
             yesterday = datetime.datetime.utcfromtimestamp(time.time())
             os.system("./transfer.py --data=" + data)
-            for band in range(0, 400, 50):
-                os.system("./transfer.py --data=" + data + "  --notrigger --novideo --start-freq=" + str(
-                    band) + " --stop-freq=" + str(band + 50))
+            #for band in range(0, 400, 50):
+            #    os.system("./transfer.py --data=" + data + "  --notrigger --novideo --start-freq=" + str(
+            #        band) + " --stop-freq=" + str(band + 50))
         else:
             print "Actual time is " + str(today) + ", waiting for the next day..."
             time.sleep(60 * 60)
