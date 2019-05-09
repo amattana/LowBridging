49ab40e91f9258c673a3907562a08b7c78ca499c
Andrea Mattana <a.mattana80@gmail.com>
2019-05-09 15:37:41 +0200

    Testing gspread mtime

--- tpm_save.py
+++ tpm_save.py
@@ -280,8 +280,8 @@ if __name__ == "__main__":
             keys, cells = read_from_google(GOOGLE_SPREADSHEET_NAME, options.station)
             write_to_local(options.station, cells)
         else:
-            #print datetime.datetime.utcfromtimestamp(os.path.getmtime("STATIONS/MAP_" + options.station + ".txt"))
-            #print datetime.datetime.strptime(mtime[:-5], "%Y-%m-%dT%H:%M:%S")
+            print "LOCAL FILE   MTIME: ", datetime.datetime.utcfromtimestamp(os.path.getmtime("STATIONS/MAP_" + options.station + ".txt"))
+            print "GSPREADSHEET MTIME: ", datetime.datetime.strptime(mtime[:-5], "%Y-%m-%dT%H:%M:%S")
             if datetime.datetime.utcfromtimestamp(os.path.getmtime("STATIONS/MAP_" + options.station + ".txt")) < \
                     datetime.datetime.strptime(mtime[:-5], "%Y-%m-%dT%H:%M:%S"):
                 print "GSpread modified, updating..."
