afcda7b5586206416000f519491501d5d77db878
Andrea Mattana <a.mattana80@gmail.com>
2019-05-16 16:51:07 +0200

    first release

---
+++ tpm_levels.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+'''
+
+  Shows TPM Levels
+
+'''
+
+__author__ = "Andrea Mattana"
+__copyright__ = "Copyright 2019, Istituto di RadioAstronomia, INAF, Italy"
+__credits__ = ["Andrea Mattana"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Andrea Mattana"
+
+import sys
+
+sys.path.append("../SKA-AAVS1/tools")
+sys.path.append("../SKA-AAVS1/tools/board")
+sys.path.append("../SKA-AAVS1/tools/pyska")
+sys.path.append("../SKA-AAVS1/tools/rf_jig")
+sys.path.append("../SKA-AAVS1/tools/config")
+sys.path.append("../SKA-AAVS1/tools/repo_utils")
+from tpm_utils import *
+from bsp.tpm import *
+
+DEVNULL = open(os.devnull, 'w')
+
+from gui_utils import *
+from rf_jig import *
+from rfjig_bsp import *
+from ip_scan import *
+
+from optparse import OptionParser
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("--board",
+                      dest="board",
+                      default="",
+                      help="The board IP")
+
+    (options, args) = parser.parse_args()
+
+    board_ip = options.board
+
+    freqs, spettro, rawdata, rms, rfpower = get_raw_meas(tpm_obj(board_ip), debug=options.debug)
+
+    print "\n\nTPM INPUT\t\tPol-X Level\tPol-Y Level\n----------------------------------------------------------"
+    for rx in xrange(len(spettro) / 2):
+        print "\nINPUT %02d\t"%(rx+1),
+        for p, pol in enumerate(["X", "Y"]):
+            print "%3.1f"%(rms[(rx*2)+p]),
+    print
+
