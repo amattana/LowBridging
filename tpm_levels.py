ca508d9a5a6696c378882516c3f9c5bbb07dc3f4
Andrea Mattana <a.mattana80@gmail.com>
2019-05-16 16:58:12 +0200

    debugging

--- tpm_levels.py
+++ tpm_levels.py
@@ -49,8 +49,8 @@ if __name__ == "__main__":
 
     print "\n\nTPM INPUT\tPol-X Level\tPol-Y Level\n----------------------------------------------------------"
     for rx in xrange(len(spettro) / 2):
-        print "\nINPUT %02d\t\t"%(rx+1),
+        print "\nINPUT %02d\t"%(rx+1),
         for p, pol in enumerate(["X", "Y"]):
-            print "%3.1f dBm\t\t"%(rms[(rx*2)+p]),
+            print "%3.1f dBm\t\t"%(rfpower[(rx*2)+p]),
     print
 
