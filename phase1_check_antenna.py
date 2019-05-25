c780481e3cb247134646f6b5274bb710e917635e
amattana <a.mattana80@gmail.com>
2019-05-25 02:50:01 +0200

    debugging

--- phase1_check_antenna.py
+++ phase1_check_antenna.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
                 print "Something went wrong!"
                 pass
 
-        if cnt == len(ant_dir):
+        if cnt/2 == len(ant_dir):
             title_left.cla()
             title_left.set_axis_off()
             title_left.plot([0.001, 0.002], color='w')
