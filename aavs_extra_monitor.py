8f74722ca1cf130e8ae890767536c1c2e72fe287
amattana <a.mattana80@gmail.com>
2019-05-29 17:16:23 +0200

    debugging

--- aavs_extra_monitor.py
+++ aavs_extra_monitor.py
@@ -259,6 +259,8 @@ def plotting_thread(directory, cadence):
 
             fig.tight_layout()#rect=[0, 0.03, 1, 0.95])
             fig.canvas.draw()
+            print tile_acq_timestamp
+            print img_dir + station_name + "/TILE-%02d_"%(tile+1) + tile_acq_timestamp[0][0] + ".svg"
             fig.savefig(img_dir + station_name + "/TILE-%02d_"%(tile+1) + tile_acq_timestamp[0][0] + ".svg")
 
 
