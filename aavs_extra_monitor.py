903f910eca03087c817cbf33a5a5331e8abf5c39
Andrea Mattana <a.mattana80@gmail.com>
2019-06-04 09:31:58 +0200

    fixed interlaced rms

--- aavs_extra_monitor.py
+++ aavs_extra_monitor.py
@@ -336,7 +336,7 @@ def plotting_thread(directory, cadence):
                 t_axes[n][2].set_xlim([0, 16])
                 t_axes[n][2].set_ylabel("RMS", fontsize=10)
                 t_axes[n][2].grid()
-                t_axes[n][2].bar(ind + 0.5, tile_rms[n * 32: n * 32 + 16], 0.8, color='b')
+                t_axes[n][2].bar(ind + 0.5, tile_rms[n * 32: (n + 1) * 32: 2], 0.8, color='b')
                 t_axes[n][2].set_title("Power Pol X", fontsize=10)
 
                 # Plot Power Y
@@ -351,7 +351,7 @@ def plotting_thread(directory, cadence):
                 t_axes[n][3].set_ylabel("RMS", fontsize=10)
                 t_axes[n][3].set_xlabel("Power Pol Y", fontsize=10)
                 t_axes[n][3].grid()
-                t_axes[n][3].bar(ind + 0.5, tile_rms[n * 32 + 16: (n+1) * 32], 0.8, color='g')
+                t_axes[n][3].bar(ind + 0.5, tile_rms[n * 32 + 1: (n+1) * 32: 2], 0.8, color='g')
 
                 t_axes[n][0].annotate("Acquisition Time (UTC)", (-17.7, -6), fontsize=12, color='black')
                 t_axes[n][0].annotate(t_timestamp, (-17.8, -12), fontsize=12, color='black')
