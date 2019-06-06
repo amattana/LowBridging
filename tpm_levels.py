808331310cc2112f19d1ec4ebfdc73a930de0bc8
amattana <a.mattana80@gmail.com>
2019-06-07 01:11:32 +0200

    fixing spaces

--- tpm_levels.py
+++ tpm_levels.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
                     print "\t%3.1f" % (adu_rms[(rx * 2) + p]),
                 else:
                     print "\t",
-            if rx % 4 == 0:
+            if (rx+1) % 4 == 0:
                 print
         print "\n\n"
 
