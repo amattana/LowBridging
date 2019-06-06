ae388c022447bf6bbe156f3ee2894cfef42475ce
amattana <a.mattana80@gmail.com>
2019-06-07 01:09:16 +0200

    fixing spaces

--- tpm_levels.py
+++ tpm_levels.py
@@ -68,14 +68,14 @@ if __name__ == "__main__":
             print "\n    #\t\t (dBm)\t\t (dBm)"
             print "\n-----------------------------------------------------"
         else:
-            print "\n\n TPM INPUT\tPol-X Level\t\tPol-Y Level"
-            print "\n    #\t\t (dBm)\tRMS\t (dBm)\tRMS"
+            print "\n\n TPM INPUT\tPol-X Level\tPol-Y Level"
+            print "\n    #\t\t  (dBm)\tRMS\t (dBm)\tRMS"
             print "\n-----------------------------------------------------"
 
         for rx in xrange(len(power_adc) / 2):
             print "\n INPUT %02d"%(rx+1),
             for p, pol in enumerate(["X", "Y"]):
-                print "\t%3.1f"%(power_rf[(rx*2)+p]),
+                print "\t  %3.1f"%(power_rf[(rx*2)+p]),
                 if options.rms:
                     print "\t%3.1f" % (adu_rms[(rx * 2) + p]),
                 else:
