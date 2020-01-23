ce6cb4bb6f701a1486b041c8c07088956da9dd08
Andrea Mattana <a.mattana80@gmail.com>
2020-01-23 15:53:53 +0100

    debugging

--- aavs_read_data.py
+++ aavs_read_data.py
@@ -32,17 +32,52 @@ def _connect_station(aavs_station):
             except:
                 continue
 
+def dt_to_timestamp(d):
+    return time.mktime(d.timetuple())
+
+
+def fname_to_tstamp(date_time_string):
+    time_parts = date_time_string.split('_')
+    d = datetime.datetime.strptime(time_parts[0], "%Y%m%d")  # "%d/%m/%Y %H:%M:%S"
+    timestamp = time.mktime(d.timetuple())
+    timestamp += int(time_parts[1])
+    return timestamp
+
+
+def ts_to_datestring(tstamp, formato="%Y-%m-%d %H:%M:%S"):
+    return datetime.datetime.strftime(datetime.datetime.fromtimestamp(tstamp), formato)
+
+
+def tstamp_to_fname(timestamp=None):
+    """
+    Returns a string date/time from a UNIX timestamp.
+    :param timestamp: A UNIX timestamp.
+    :return: A date/time string of the form yyyymmdd_secs
+    """
+    if timestamp is None:
+        timestamp = 0
+
+    datetime_object = datetime.datetime.fromtimestamp(timestamp)
+    hours = datetime_object.hour
+    minutes = datetime_object.minute
+    seconds = datetime_object.second
+    full_seconds = seconds + (minutes * 60) + (hours * 60 * 60)
+    full_seconds_formatted = format(full_seconds, '05')
+    base_date_string = datetime.datetime.fromtimestamp(timestamp).strftime('%Y%m%d')
+    full_date_string = base_date_string + '_' + str(full_seconds_formatted)
+    return str(full_date_string)
+
 
 if __name__ == "__main__":
     from optparse import OptionParser
     from sys import argv, stdout
 
-    parser = OptionParser(usage="usage: %monitor_bandpasses [options]")
+    parser = OptionParser(usage="usage: %aavs_read_data [options]")
     parser.add_option("--config", action="store", dest="config",
-                      default="/opt/aavs/config/aavs1_full_station.yml",
+                      default="/opt/aavs/config/aavs2.yml",
                       help="Station configuration files to use, comma-separated (default: AAVS1)")
     parser.add_option("--directory", action="store", dest="directory",
-                      default="/storage/monitoring/integrated_data",
+                      default="/storage/monitoring/integrated_data/",
                       help="Directory where plots will be generated (default: /storage/monitoring/integrated_data)")
     parser.add_option("--tile", action="store", dest="tile", type=int,
                       default=1, help="Tile Number")
@@ -52,18 +87,49 @@ if __name__ == "__main__":
                       default="", help="Start time for filter (YYYY-mm-DD_HH:MM:SS)")
     parser.add_option("--stop", action="store", dest="stop",
                       default="", help="Stop time for filter (YYYY-mm-DD_HH:MM:SS)")
+    parser.add_option("--date", action="store", dest="date",
+                      default="", help="Stop time for filter (YYYY-mm-DD)")
 
     (opts, args) = parser.parse_args(argv[1:])
+
+    t_date = None
+    t_start = None
+    t_stop = None
+    t_cnt = 0
+
+    if opts.date:
+        try:
+            t_date = datetime.datetime.strptime(opts.date, "%Y-%m-%d")
+            t_start = dt_to_timestamp(t_date)
+            t_stop = dt_to_timestamp(t_date) + (60 * 60 * 24)
+        except:
+            print "Bad date format detected (must be YYYY-MM-DD)"
+    else:
+        if opts.start:
+            try:
+                t_start = dt_to_timestamp(datetime.datetime.strptime(opts.start, "%Y-%m-%d_%H:%M:%S"))
+            except:
+                print "Bad t_start time format detected (must be YYYY-MM-DD_HH:MM:SS)"
+        if opts.stop:
+            try:
+                t_stop = dt_to_timestamp(datetime.datetime.strptime(opts.stop, "%Y-%m-%d_%H:%M:%S"))
+            except:
+                print "Bad t_stop time format detected (must be YYYY-MM-DD_HH:MM:SS)"
 
     plt.ioff()
     nplot = 16
     ind = np.arange(nplot)
+
     # Load configuration file
     station.load_configuration_file(opts.config)
     station_name = station.configuration['station']['name']
-    file_manager = ChannelFormatFileManager(root_path=opts.directory, daq_mode=FileDAQModes.Integrated)
-    dic = file_manager.get_metadata(tile_id=int(opts.tile)-1)
-    data = file_manager.read_data(tile_id=int(opts.tile)-1, n_samples=dic['n_blocks'])
+    print "\nStation Name: ", station_name
+    print "Checking directory: ", opts.directory+station_name.lower() + "\n"
+    file_manager = ChannelFormatFileManager(root_path=opts.directory+station_name.lower(),
+                                            daq_mode=FileDAQModes.Integrated)
+    lista = sorted(glob.glob(opts.directory + station_name.lower() + "/channel_integ_%d_*hdf5" % (int(opts.tile)-1)))
+
+    #data = file_manager.read_data(tile_id=int(opts.tile)-1, n_samples=dic['n_blocks'])
 
     assex = np.linspace(0, 400, len(data[0][:, 0, 0, -1]))
 
@@ -73,9 +139,8 @@ if __name__ == "__main__":
 
     base, x, y = get_antenna_positions(station_name)
     ants = []
-    for j in range(16*nof_tiles):
-        ants += ["ANT-%03d" % int(base[j])]
-
+    for j in base:
+        ants += ["ANT-%03d" % int(j)]
     print
     ax = []
     fig = plt.figure(figsize=(11, 7), facecolor='w')
@@ -136,39 +201,51 @@ if __name__ == "__main__":
         ax[i].set_title("IN " + str(i + 1), fontsize=8)
 
     # Draw antenna positions
-    for en in range(16):
+    for en in range(nplot):
         ax_top_map.plot(float(x[en + (int(opts.tile - 1) * 16)]), float(y[en + (int(opts.tile - 1) * 16)]),
                         marker='+', markersize=4, linestyle='None', color='k')
 
-    for bl in range(dic['n_blocks']):
-        try:
-            if bl > opts.skip:
-                for ant in range(nplot):
-                    ax[ant].cla()
-                    with np.errstate(divide='ignore'):
-                        spettro = 10 * np.log10(data[0][:, ant, 0, bl])
-                    ax[ant].plot(assex[2:-1], spettro[2:-1], scaley=True, color='b')
-                    with np.errstate(divide='ignore'):
-                        spettro = 10 * np.log10(data[0][:, ant, 1, bl])
-                    ax[ant].plot(assex[2:-1], spettro[2:-1], scaley=True, color='g')
-                    ax[ant].set_ylim(0, 50)
-                    ax[ant].set_xlim(0, 400)
-                    ax[ant].set_title("IN " + str(ant + 1), fontsize=8)
+    for l in lista:
+        dic = file_manager.get_metadata(timestamp=fname_to_tstamp(l[-21:-7]), tile_id=(int(opts.tile)-1))
+        if dic:
+            data, timestamps = file_manager.read_data(timestamp=fname_to_tstamp(l[-21:-7]), tile_id=int(opts.tile)-1,
+                                                      n_samples=dic['n_blocks'])
+            cnt = 0
+            if not t_start >= timestamps[-1]:
+                if not t_stop <= timestamps[0]:
+                    for i, t in enumerate(timestamps):
+                        if t_start <= t[0] <= t_stop:
+                            cnt = cnt + 1
+                            t_cnt = t_cnt + 1
 
-                ax_top_tile.cla()
-                ax_top_tile.set_axis_off()
-                ax_top_tile.plot([0.001, 0.002], color='w')
-                ax_top_tile.set_xlim(-20, 20)
-                ax_top_tile.set_ylim(-20, 20)
-                ax_top_tile.annotate("TILE %02d" % (opts.tile), (-12, 6), fontsize=24, color='black')
-                orario = datetime.datetime.strftime(datetime.datetime.utcfromtimestamp(data[1][bl]), "%Y-%m-%d  %H:%M:%S")
-                ax_top_tile.annotate(orario, (-18, -12), fontsize=12, color='black')
-                orario = datetime.datetime.strftime(datetime.datetime.utcfromtimestamp(data[1][bl]), "%Y-%m-%d_%H%M%S")
+                            # Generate picture
+                            for ant in range(nplot):
+                                ax[ant].cla()
+                                with np.errstate(divide='ignore'):
+                                    spettro = 10 * np.log10(data[:, ant, 0, i])
+                                ax[ant].plot(assex[2:-1], spettro[2:-1], scaley=True, color='b')
+                                with np.errstate(divide='ignore'):
+                                    spettro = 10 * np.log10(data[:, ant, 1, i])
+                                ax[ant].plot(assex[2:-1], spettro[2:-1], scaley=True, color='g')
+                                ax[ant].set_ylim(0, 50)
+                                ax[ant].set_xlim(0, 400)
+                                ax[ant].set_title("IN " + str(ant + 1), fontsize=8)
 
-                plt.savefig("/storage/monitoring/pictures/TILE-%02d"%(opts.tile) + "/TILE-%02d_"%(opts.tile)+orario+".png")
-                sys.stdout.write("\r[%d/%d] Generated picture for TILE-%02d timestamp "%(bl+1, int(dic['n_blocks']), opts.tile) + orario)
-                sys.stdout.flush()
-        except:
-            print "Tile-%02d "%(opts.tile), orario, "exception raised...\n"
+                            ax_top_tile.cla()
+                            ax_top_tile.set_axis_off()
+                            ax_top_tile.plot([0.001, 0.002], color='w')
+                            ax_top_tile.set_xlim(-20, 20)
+                            ax_top_tile.set_ylim(-20, 20)
+                            ax_top_tile.annotate("TILE %02d" % opts.tile, (-12, 6), fontsize=24, color='black')
+                            orario = ts_to_datestring(t[0])
+                            ax_top_tile.annotate(orario, (-18, -12), fontsize=12, color='black')
+                            orario = ts_to_datestring(t[0], formato="%Y-%m-%d_%H%M%S")
 
-    print
+                            plt.savefig("/storage/monitoring/pictures/TILE-%02d" % opts.tile +
+                                        "/TILE-%02d_" % opts.tile + orario + ".png")
+
+            print l[-21:-7], "\t", ts_to_datestring(timestamps[0][0]), "\t", \
+                ts_to_datestring(timestamps[-1][0]), "\t", cnt
+        else:
+            print l[-21:-7], ": no metadata available"
+
