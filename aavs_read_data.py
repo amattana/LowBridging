89c6b440c1e337017c643485d7aa4f552cbde88d
amattana <a.mattana80@gmail.com>
2020-02-25 20:12:25 +0100

    Fixing weather

--- aavs_read_data.py
+++ aavs_read_data.py
@@ -610,6 +610,7 @@ if __name__ == "__main__":
             z_rain = []
             for n, t in enumerate(t_stamps):
                 print len(t_stamps), n, t, ts_to_datestring(t)
+                sleep(0.2)
                 if not closest(np.array(w_time), t) == w_time[-1]:
                     z_temp += [calc_value(w_time, w_temp, t)]
                     z_wind += [calc_value(w_time, w_wind, t)]
