fcb86aa7d582d0f98963a9b33ddeca9bcc4cc27c
amattana <a.mattana80@gmail.com>
2020-02-25 17:54:41 +0100

    Fixing weather

--- aavs_utils.py
+++ aavs_utils.py
@@ -125,7 +125,7 @@ def eq_retta(x1, y1, x2, y2):
 def calc_value(serie_x, serie_y, x):
     x1 = closest(np.array(serie_x), x)
     x2 = x1 + 1
-    print " * ", len(serie_x), len(serie_y), x1, x2
+    #print " * ", len(serie_x), len(serie_y), x1, x2
     return eq_retta(x1, serie_y[x1], x2, serie_y[x2])(x)
 
 
