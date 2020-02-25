c6b72856ee3a86e6d30aedaa38bf3c5f5a97c794
amattana <a.mattana80@gmail.com>
2020-02-25 17:21:31 +0100

    Testing weather

--- aavs_read_data.py
+++ aavs_read_data.py
@@ -609,7 +609,7 @@ if __name__ == "__main__":
             z_wdir = []
             z_rain = []
             for t in t_stamps:
-                if not closest(w_time, t) == w_time[-1]:
+                if not closest(np.array(w_time), t) == w_time[-1]:
                     z_temp += [calc_value(w_time, w_temp, t)]
                     z_wind += [calc_value(w_time, w_wind, t)]
                     z_wdir += [calc_value(w_time, w_wdir, t)]
