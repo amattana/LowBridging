b50583b359d4cbb5e7e0f1298b4955aec953e23a
amattana <a.mattana80@gmail.com>
2020-03-04 14:54:03 +0100

    Trying wind direction

--- aavs_read_data.py
+++ aavs_read_data.py
@@ -737,10 +737,10 @@ if __name__ == "__main__":
                 #     print a, angle_wdir[a], x_tick[a], y, x_tick[a] + xs, y + ys, r
                     m = MarkerStyle(">")
                     m._transform.rotate_deg(angle_wdir[a])
-                    ax_wind.scatter(x_tick[a], y, marker=m, s=100, color='orchid')
+                    ax_wind.scatter(x_tick[a], y, marker=m, s=100, color='g')
                     m = MarkerStyle("_")
                     m._transform.rotate_deg(angle_wdir[a])
-                    ax_wind.scatter(x_tick[a], y, marker=m, s=500, color='orchid')
+                    ax_wind.scatter(x_tick[a], y, marker=m, s=500, color='g')
                 fig.subplots_adjust(right=0.9)
 
 
