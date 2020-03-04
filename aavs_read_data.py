9602e76f85b4eb7aa63798980af4cad9aeecaa78
amattana <a.mattana80@gmail.com>
2020-03-04 16:12:42 +0100

    changing wind dir color

--- aavs_read_data.py
+++ aavs_read_data.py
@@ -695,10 +695,10 @@ if __name__ == "__main__":
                 #     print a, angle_wdir[a], x_tick[a], y, x_tick[a] + xs, y + ys, r
                     m = MarkerStyle(">")
                     m._transform.rotate_deg(angle_wdir[a])
-                    ax_wind.scatter(z_tick[a], y, marker=m, s=100, color='k')
+                    ax_wind.scatter(z_tick[a], y, marker=m, s=100, color='gray')
                     m = MarkerStyle("_")
                     m._transform.rotate_deg(angle_wdir[a])
-                    ax_wind.scatter(z_tick[a], y, marker=m, s=400, color='k')
+                    ax_wind.scatter(z_tick[a], y, marker=m, s=400, color='gray')
                 fig.subplots_adjust(right=0.8)
 
             else:
