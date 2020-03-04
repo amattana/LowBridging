e8eca8fe37a6c82d4740ccaad034afd23f94dcd7
amattana <a.mattana80@gmail.com>
2020-03-04 17:30:06 +0100

    added power plot

--- aavs_read_data.py
+++ aavs_read_data.py
@@ -880,58 +880,55 @@ if __name__ == "__main__":
             z_wdir = []
             z_rain = []
             for n, t in enumerate(t_stamps):
-                #print len(t_stamps), n, t, ts_to_datestring(t)
-                #sleep(1)
-                #if not closest(np.array(w_time), t) == w_time[-1]:
                 z_temp += [calc_value(w_time, w_temp, t)]
                 #print " * ", ts_to_datestring(t), calc_value(w_time, w_temp, t)
                 z_wind += [calc_value(w_time, w_wind, t)]
                 z_wdir += [calc_value(w_time, w_wdir, t)]
                 z_rain += [calc_value(w_time, w_rain, t)]
 
-                ax_weather.set_ylabel('Temperature (C)', color='r')
-                ax_weather.set_xlim(t_stamps[0], t_stamps[-1])
-                ax_weather.set_ylim(15, 45)
-                ax_weather.set_yticks(np.arange(15, 50, 5))
-                ax_weather.set_yticklabels(np.arange(15, 50, 5), color='r')
-                ax_weather.grid()
-                x_tick = []
-                y_wdir = []
-                angle_wdir = []
-                step = orari[0].hour
-                for z in range(len(orari)):
-                    if orari[z].hour == step:
-                        x_tick += [t_stamps[z]]
-                        y_wdir += [z_wind[z]]
-                        angle_wdir += [z_wdir[z]]
-                        step = step + 1
-                x_tick += [t_stamps[len(orari)]]
-                ax_weather.set_xticks(x_tick)
-                ax_weather.set_xticklabels((np.array(range(0, len(x_tick), 1)) + orari[0].hour).astype("str").tolist())
+            ax_weather.set_ylabel('Temperature (C)', color='r')
+            ax_weather.set_xlim(t_stamps[0], t_stamps[-1])
+            ax_weather.set_ylim(15, 45)
+            ax_weather.set_yticks(np.arange(15, 50, 5))
+            ax_weather.set_yticklabels(np.arange(15, 50, 5), color='r')
+            ax_weather.grid()
+            x_tick = []
+            y_wdir = []
+            angle_wdir = []
+            step = orari[0].hour
+            for z in range(len(orari)):
+                if orari[z].hour == step:
+                    x_tick += [t_stamps[z]]
+                    y_wdir += [z_wind[z]]
+                    angle_wdir += [z_wdir[z]]
+                    step = step + 1
+            x_tick += [t_stamps[len(orari)]]
+            ax_weather.set_xticks(x_tick)
+            ax_weather.set_xticklabels((np.array(range(0, len(x_tick), 1)) + orari[0].hour).astype("str").tolist())
 
-                ax_wind = ax_weather.twinx()
-                ax_wind.plot(t_stamps[:len(z_temp)], z_wind, color='b', lw=1.5)
-                ax_wind.set_ylim(0, 60)
-                ax_wind.set_ylabel('Wind (Km/h)', color='b')
-                ax_wind.tick_params(axis='y', labelcolor='b')
+            ax_wind = ax_weather.twinx()
+            ax_wind.plot(t_stamps[:len(z_temp)], z_wind, color='b', lw=1.5)
+            ax_wind.set_ylim(0, 60)
+            ax_wind.set_ylabel('Wind (Km/h)', color='b')
+            ax_wind.tick_params(axis='y', labelcolor='b')
 
-                ax_rain = ax_weather.twinx()
-                ax_rain.plot(t_stamps[:len(z_temp)], z_rain, color='g', lw=1.5)
-                ax_rain.set_ylim(0, 20)
-                ax_rain.set_ylabel('Rain (mm)', color='g')
-                ax_rain.tick_params(axis='y', labelcolor='g')
-                ax_rain.spines["right"].set_position(("axes", 1.06))
-                ax_weather.plot(t_stamps[:len(z_temp)], z_temp, color='r', lw=1.5)
+            ax_rain = ax_weather.twinx()
+            ax_rain.plot(t_stamps[:len(z_temp)], z_rain, color='g', lw=1.5)
+            ax_rain.set_ylim(0, 20)
+            ax_rain.set_ylabel('Rain (mm)', color='g')
+            ax_rain.tick_params(axis='y', labelcolor='g')
+            ax_rain.spines["right"].set_position(("axes", 1.06))
+            ax_weather.plot(t_stamps[:len(z_temp)], z_temp, color='r', lw=1.5)
 
-                # Draw wind direction
-                for a, y in enumerate(y_wdir):
-                    m = MarkerStyle(">")
-                    m._transform.rotate_deg(angle_wdir[a])
-                    ax_wind.scatter(x_tick[a], y, marker=m, s=100, color='g')
-                    m = MarkerStyle("_")
-                    m._transform.rotate_deg(angle_wdir[a])
-                    ax_wind.scatter(x_tick[a], y, marker=m, s=500, color='g')
-                fig.subplots_adjust(right=0.9)
+            # Draw wind direction
+            for a, y in enumerate(y_wdir):
+                m = MarkerStyle(">")
+                m._transform.rotate_deg(angle_wdir[a])
+                ax_wind.scatter(x_tick[a], y, marker=m, s=100, color='g')
+                m = MarkerStyle("_")
+                m._transform.rotate_deg(angle_wdir[a])
+                ax_wind.scatter(x_tick[a], y, marker=m, s=500, color='g')
+            fig.subplots_adjust(right=0.9)
 
         if not os.path.exists(POWER_PATH):
             os.makedirs(POWER_PATH)
