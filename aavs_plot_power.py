aa123b415e1b5062d6747a3be556be584b360bb0
amattana <a.mattana80@gmail.com>
2020-03-12 16:27:00 +0100

    Change equalization to zero dB

--- aavs_plot_power.py
+++ aavs_plot_power.py
@@ -72,17 +72,20 @@ if __name__ == "__main__":
             ax.cla()
             xmin = full_time[0][0]
             xmax = full_time[-1][-1]
-            #ymin = np.ceil(np.mean(full_data[0]) - 5)
-            #ymax = np.ceil(np.mean(full_data[0]) + 5)
+            ymin = np.ceil(np.mean(full_data[0]) - 5)
+            ymax = np.ceil(np.mean(full_data[0]) + 5)
             for n, l in enumerate(lista):
                 ax.plot(full_time[n], full_data[n], label=l.split("/")[-1])
                 xmin = min(full_time[n][0], xmin)
                 xmax = max(full_time[n][-1], xmax)
-                #ymin = min(np.ceil(np.mean(full_data[n])) - 5, ymin)
-                #ymax = max(np.ceil(np.mean(full_data[n])) + 6, ymax)
+                ymin = min(np.ceil(np.mean(full_data[n])) - 5, ymin)
+                ymax = max(np.ceil(np.mean(full_data[n])) + 6, ymax)
             #print xmin, xmax, ymin, ymax
             ax.set_xlim(xmin, xmax)
-            ax.set_ylim(-6, 6)
+            if opts.eq:
+                ax.set_ylim(-14, 2)
+            else:
+                ax.set_ylim(ymin, ymax)
             ax.set_xlabel("UTC Time", fontsize=14)
             ax.set_ylabel("dB", fontsize=14)
             ax.grid()
