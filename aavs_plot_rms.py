e7f58ccc021442631c87bc46e37827738f138ece
amattana <a.mattana80@gmail.com>
2020-03-27 14:45:24 +0100

    Added weather

--- aavs_plot_rms.py
+++ aavs_plot_rms.py
@@ -2,9 +2,9 @@ from matplotlib import pyplot as plt
 import os
 import numpy as np
 from matplotlib.gridspec import GridSpec
-from aavs_utils import ts_to_datestring, dt_to_timestamp
 import datetime
 import glob
+from aavs_utils import dt_to_timestamp, ts_to_datestring, mro_daily_weather, diclist_to_array
 
 t_start = 0
 t_stop = 0
@@ -68,7 +68,8 @@ if __name__ == "__main__":
                       default="", help="List of signals to plot, a string where tuple element are comma separated 'tile-input-pol,tile-input-pol'")
     parser.add_option("--date", action="store", dest="date",
                       default="all", help="Date in YYYY-MM-DD (required, default 'all')")
-
+    parser.add_option("--weather", action="store_true", dest="weather",
+                      default=False, help="Add weather info (if available)")
     (opts, args) = parser.parse_args(argv[1:])
 
     try:
@@ -86,6 +87,20 @@ if __name__ == "__main__":
     except:
         print "Wrong date format or missing required argument (" + opts.date + ")"
         exit(1)
+
+    w_data = []
+    if opts.weather:
+        w_units, w_data = mro_daily_weather(start=ts_to_datestring(t_start, formato="%Y-%m-%d_%H:%M:%S"),
+                                             stop=ts_to_datestring(t_stop, formato="%Y-%m-%d_%H:%M:%S"))
+        if len(w_data):
+            w_time = diclist_to_array(w_data, 'time')
+            w_temp = diclist_to_array(w_data, 'temp')
+            w_wind = diclist_to_array(w_data, 'wind')
+            w_wdir = diclist_to_array(w_data, 'wdir')
+            w_rain = diclist_to_array(w_data, 'rain')
+            print "\nWeather data acquired, %d records"%len(w_temp)#, "  ", w_temp[0:8]
+        else:
+            print "\nNo weather data available\n"
 
     plt.ion()
 
@@ -132,6 +147,43 @@ if __name__ == "__main__":
         ax.set_xlabel("UTC Time (hours)")
         ax.set_title("ADC RMS     Start Time: %s      End Time: %s" % (ts_to_datestring(x[0]), ts_to_datestring(x[-1])))
         ax.legend(markerscale=8)
+
+        if len(w_data):
+            ax_weather = ax.twinx()
+            ax_weather.set_ylabel('Temperature (C)', color='r')
+            #ax_weather.set_xlim(t_stamps[0], t_stamps[-1])
+            ax_weather.set_ylim(50, 0)
+            ax_weather.set_yticks(np.arange(15, 50, 5))
+            ax_weather.set_yticklabels(np.arange(15, 50, 5), color='r')
+
+            ax_wind = ax_power.twinx()
+            ax_wind.plot(w_time, w_wind, color='orange', lw=1.5)
+            ax_wind.set_ylim(0, 80)
+            ax_wind.set_ylabel('Wind (Km/h)', color='orange')
+            ax_wind.tick_params(axis='y', labelcolor='orange')
+            ax_wind.spines["right"].set_position(("axes", 1.06))
+
+            ax_rain = ax_power.twinx()
+            ax_rain.plot(w_time, w_rain, color='cyan', lw=1.5)
+            ax_rain.set_ylim(0, 100)
+            ax_rain.set_ylabel('Rain (mm)', color='cyan')
+            ax_rain.tick_params(axis='y', labelcolor='cyan')
+            ax_rain.spines["right"].set_position(("axes", 1.12))
+            ax_weather.plot(w_time, w_temp, color='r', lw=1.5)
+
+            # Draw wind direction
+            for a in range(len(w_wdir)):
+                if not a % 4:
+                    m = MarkerStyle(">")
+                    m._transform.rotate_deg(w_wdir[a])
+                    # print a, xticks[a], w_wind[a], len(xticks), len(w_wind)
+                    ax_wind.scatter(w_time[a], w_wind[a], marker=m, s=100, color='orchid')
+                    m = MarkerStyle("_")
+                    m._transform.rotate_deg(w_wdir[a])
+                    ax_wind.scatter(w_time[a], w_wind[a], marker=m, s=500, color='orchid')
+            fig.subplots_adjust(right=0.86)
+
+
         plt.show()
 
     else:
