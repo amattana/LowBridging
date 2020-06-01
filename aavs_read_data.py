ed35198b5d46720caa6f9b57068843ab73dca70d
Andrea Mattana <andrea.mattana@inaf.it>
2020-06-01 14:16:53 +0200

    spec_analyzer max xticks

--- aavs_read_data.py
+++ aavs_read_data.py
@@ -1227,6 +1227,7 @@ if __name__ == "__main__":
         ax.grid()
         if opts.xticks:
             ax.set_xticks(range(0, 450, 1))
+            ax.set_xticklabels(range(0, 450, 1), rotation=45, fontsize='small')
         else:
             ax.set_xticks(range(0, 450, 50))
         ax.set_xlim(int(opts.startfreq), int(opts.stopfreq))
@@ -1269,6 +1270,7 @@ if __name__ == "__main__":
         ax.grid()
         if opts.xticks:
             ax.set_xticks(range(0, 450, 1))
+            ax.set_xticklabels(range(0, 450, 1), rotation=45, fontsize='small')
         else:
             ax.set_xticks(range(0, 450, 50))
         ax.set_xlim(int(opts.startfreq), int(opts.stopfreq))
