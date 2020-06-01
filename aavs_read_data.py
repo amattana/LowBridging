86cbb5af913fc7a4e7726a5cf01a22f42862693e
Andrea Mattana <andrea.mattana@inaf.it>
2020-06-01 17:10:34 +0200

    oplot freq range

--- aavs_read_data.py
+++ aavs_read_data.py
@@ -1327,7 +1327,7 @@ if __name__ == "__main__":
                 OPLOT_PATH + "/" + station_name + "/" + date_path + "/TILE-%02d_ANT-%03d" % (int(tile), int(skala_name))):
             os.makedirs(OPLOT_PATH + "/" + station_name + "/" + date_path + "/TILE-%02d_ANT-%03d" % (int(tile), int(skala_name)))
 
-        grid = GridSpec(15, 8, hspace=1, wspace=0.4, left=0.08, right=0.98, bottom=0.1, top=0.98)
+        grid = GridSpec(15, 8, hspace=1.2, wspace=0.4, left=0.08, right=0.98, bottom=0.1, top=0.98)
         fig = plt.figure(figsize=(11, 7), facecolor='w')
 
         ax_top_map = fig.add_subplot(grid[0:3, 7])
@@ -1367,7 +1367,7 @@ if __name__ == "__main__":
         ax_xpol.set_ylabel("dB", fontsize=12)
         if opts.xticks:
             ax_xpol.set_xticks(np.arange(len(asse_x)))
-            ax_xpol.set_xticklabels(["%3.1f"%s for s in asse_x], fontsize=4, rotation=90)
+            ax_xpol.set_xticklabels(["%3.1f"%s for s in asse_x], fontsize=3, rotation=90)
         else:
             ax_xpol.set_xticks([x*64 for x in range(9)])
             ax_xpol.set_xticklabels([x*50 for x in range(9)], fontsize=10)
@@ -1382,7 +1382,7 @@ if __name__ == "__main__":
         ax_ypol.set_ylabel("dB", fontsize=12)
         if opts.xticks:
             ax_ypol.set_xticks(np.arange(len(asse_x)))
-            ax_ypol.set_xticklabels(["%3.1f"%s for s in asse_x], fontsize=4, rotation=90)
+            ax_ypol.set_xticklabels(["%3.1f"%s for s in asse_x], fontsize=3, rotation=90)
         else:
             ax_ypol.set_xticks([x*64 for x in range(9)])
             ax_ypol.set_xticklabels([x*50 for x in range(9)], fontsize=10)
