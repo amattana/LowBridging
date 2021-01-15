f6cde33868570b48e90863a0f36fcd0508eac020
Andrea Mattana <andrea.mattana@inaf.it>
2021-01-15 18:43:39 +0100

    Added AAVS1 processing

--- aavs1_process_contchans.py
+++ aavs1_process_contchans.py
@@ -7,7 +7,7 @@ import datetime
 from aavs_utils import ts_to_datestring, tstamp_to_fname, dt_to_timestamp, fname_to_tstamp
 import os
 import numpy as np
-
+from tqdm import tqdm
 
 conf = "/opt/aavs/config/aavs1_full_station.yml"
 station.load_configuration_file(conf)
@@ -15,15 +15,15 @@ station_name = station.configuration['station']['name']
 modo = FileDAQModes.Continuous
 file_manager = ChannelFormatFileManager(root_path="/data/data_2/2019_03_25_204_24hr/", daq_mode=modo)
 tiles = range(16)
-for t in tiles:
+for n, t in tqdm(enumerate(tiles)):
     lista = sorted(glob.glob("/data/data_2/2019_03_25_204_24hr/channel_cont_%d_*hdf5" % t))
     for l in lista:
         dic = file_manager.get_metadata(timestamp=fname_to_tstamp(l[-21:-7]), tile_id=t)
         data, timestamps = file_manager.read_data(timestamp=fname_to_tstamp(l[-21:-7]), tile_id=t, n_samples=20000000)
-        print "Timestamp: %s " % (ts_to_datestring(timestamps[0]))
+        #print "Timestamp: %s " % (ts_to_datestring(timestamps[0]))
         for ant in range(16):
-            sys.stdout.write("\rProcessing TILE-%02d ANT-%03d" % (t + 1, ant + 1))
-            sys.stdout.flush()
+            #sys.stdout.write("\rProcessing TILE-%02d ANT-%03d" % (t + 1, ant + 1))
+            #sys.stdout.flush()
             d = data[0, ant, 0, :]
             x = 10 * np.log10(np.abs(np.complex(np.sum(np.transpose(d.tolist())[0]),
                                                 np.sum(np.transpose(d.tolist())[1]))))
