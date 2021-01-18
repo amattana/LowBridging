bf567500e9ab776a9659a91b8ebbfb62bd544f40
Andrea Mattana <andrea.mattana@inaf.it>
2021-01-18 11:21:07 +0100

    Fixed Samples averaging before abs

--- aavs1_process_contchans.py
+++ aavs1_process_contchans.py
@@ -33,11 +33,9 @@ for t in tiles:
             #sys.stdout.write("\rProcessing TILE-%02d ANT-%03d" % (t + 1, ant + 1))
             #sys.stdout.flush()
             d = data[0, ant, 0, :]
-            x = 10 * np.log10(np.abs(np.complex(np.sum(np.transpose(d.tolist())[0]),
-                                                np.sum(np.transpose(d.tolist())[1]))))
+            x = 10 * np.log10(np.abs((np.complex(np.sum(np.transpose(d.tolist())[0]), np.sum(np.transpose(d.tolist())[1]))/len(d))))
             d = data[0, ant, 1, :]
-            y = 10 * np.log10(np.abs(np.complex(np.sum(np.transpose(d.tolist())[0]),
-                                                np.sum(np.transpose(d.tolist())[1]))))
+            y = 10 * np.log10(np.abs((np.complex(np.sum(np.transpose(d.tolist())[0]), np.sum(np.transpose(d.tolist())[1]))/len(d))))
             with open("/storage/monitoring/aavs1_data/AAVS1_TILE-%02d_ANT-%03d_Pol-X.txt" % (t + 1,  ant + 1), "a") as f:
                 f.write("%d\t%f\n" % (timestamps[0], x))
                 f.flush()
