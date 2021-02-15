1a59135b4481ba117747cb6cbf677aff6a0c1564
Andrea Mattana <andrea.mattana@inaf.it>
2021-02-15 19:11:48 +0100

    A tool to plot a Gannt chart

--- aavs_gannt.py
+++ aavs_gannt.py
@@ -5,11 +5,13 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 import matplotlib.dates
+import glob
 from matplotlib.dates import WEEKLY, MONTHLY, DateFormatter, rrulewrapper, RRuleLocator
 import numpy as np
 import os
 import datetime
 import calendar
+from matplotlib.gridspec import GridSpec
 
 def _create_date(datetxt):
     """Creates the date"""
@@ -19,13 +21,18 @@ def _create_date(datetxt):
     return mdate
 
 
+def ts_to_datestring(tstamp, formato="%Y-%m-%d %H:%M:%S"):
+    return datetime.datetime.strftime(datetime.datetime.utcfromtimestamp(tstamp), formato)
+
+
 def read_records(fpath):
     taskes = []
     if os.path.exists(fpath):
-        for ant_file in os.listdir(fpath):
+        file_list = sorted(glob.glob(fpath + "*.txt"))
+        for ant_file in file_list:
             record = {}
-            record['name'] = ant_file
-            with open(fpath + "/" + ant_file) as f:
+            record['name'] = ant_file[len(fpath):]
+            with open(ant_file) as f:
                 data = f.readlines()
             activity = []
             for d in data:
@@ -33,8 +40,9 @@ def read_records(fpath):
                     d_start = d.split(",")[0].split()[0] + " " + d.split(",")[0].split()[1]
                     d_stop = d.split(",")[1].split()[0] + " " + d.split(",")[1].split()[1]
                     activity += [(calendar.timegm(datetime.datetime.strptime(d_start, "%Y-%m-%d %H:%M:%S").timetuple()),
-                                calendar.timegm(datetime.datetime.strptime(d_stop, "%Y-%m-%d %H:%M:%S").timetuple()) -
-                                calendar.timegm(datetime.datetime.strptime(d_start, "%Y-%m-%d %H:%M:%S").timetuple()))]
+                                  calendar.timegm(datetime.datetime.strptime(d_stop, "%Y-%m-%d %H:%M:%S").timetuple()) -
+                                  calendar.timegm(datetime.datetime.strptime(d_start, "%Y-%m-%d %H:%M:%S").timetuple())),
+                                 d.split(",")[2].split()[0]]
             record['events'] = activity
             taskes += [record]
     return taskes
@@ -51,41 +59,56 @@ def CreateGanttChart(fpath):
         customDates = []
         #
         taskes = read_records(fpath)
-
+        xmin = taskes[0]['events'][0][0]
+        xmax = taskes[-1]['events'][0][0] + taskes[-1]['events'][0][1]
+        #print xmin, xmax
         ilen = len(taskes)
-        pos = np.arange(0.5, ilen * 0.5 + 0.5, 0.5)
+        #print ilen
+        pos = np.arange(0.5, ilen * 1 + 0.5)
+        #print pos
         task_dates = {}
         #for i, task in enumerate(ylabels):
         #    task_dates[task] = customDates[i]
         for t in taskes:
-            ylabels += [t['name']]
-        fig = plt.figure(figsize=(20, 8))
-        ax = fig.add_subplot(111)
+            ylabels += [t['name'][2:-4]]
+        gs = GridSpec(2, 1, hspace=1.5, wspace=0.1, left=0.25, right=0.9, bottom=0.25, top=0.9)
+        fig = plt.figure(figsize=(12, 3))
+        ax = fig.add_subplot(gs[0:2, 0])
+        #print taskes,
         for n, t in enumerate(taskes):
-            #start_date, end_date = task_dates[ylabels[i]]
-            ax.broken_barh(t['events'], (n, 0.8), edgecolor='lightgreen', color='orange', alpha=0.8)
+            #print t['events'], [t['events'][0]], t['events'][1]
+            ax.broken_barh([t['events'][0]], (n+0.1, 0.9), edgecolor='w', color=t['events'][1], alpha=0.9, label=t['name'])
+            if n == 4:
+                break
         locsy, labelsy = plt.yticks(pos, ylabels)
-        plt.setp(labelsy, fontsize=14)
+        plt.setp(labelsy, fontsize=10)
         #    ax.axis('tight')
-        ax.set_ylim(ymin=-0.1, ymax=ilen * 0.5 + 0.5)
+        ax.set_ylim(ymin=-0.1, ymax=ilen * 1 + 0.5)
         ax.grid(color='g', linestyle=':')
-        ax.xaxis_date()
-        rule = rrulewrapper(WEEKLY, interval=1)
-        loc = RRuleLocator(rule)
+        #ax.xaxis_date()
+        #rule = rrulewrapper(WEEKLY, interval=1)
+        #loc = RRuleLocator(rule)
         # formatter = DateFormatter("%d-%b '%y")
-        formatter = DateFormatter("%d-%b")
+        #formatter = DateFormatter("%d-%b")
 
-        ax.xaxis.set_major_locator(loc)
-        ax.xaxis.set_major_formatter(formatter)
-        labelsx = ax.get_xticklabels()
-        plt.setp(labelsx, rotation=30, fontsize=10)
+        #ax.xaxis.set_major_locator(loc)
+        #ax.xaxis.set_major_formatter(formatter)
+        #labelsx = ax.get_xticklabels()
+        #plt.setp(labelsx, rotation=30, fontsize=8)
 
         font = font_manager.FontProperties(size='small')
-        ax.legend(loc=1, prop=font)
+        #ax.legend(loc=1, prop=font)
 
+        ax.set_xlim(xmin, xmax)
+        xticks = [xt['events'][0][0] for xt in taskes]
+        xticks += [taskes[-1]['events'][0][0] + taskes[-1]['events'][0][1]]
+        xtickslabel = [ts_to_datestring(xt, "%Y-%m") for xt in xticks]
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(xtickslabel, fontsize=10)
         ax.invert_yaxis()
         fig.autofmt_xdate()
-        plt.savefig('gantt.svg')
+        #plt.tight_layout()
+        plt.savefig('gantt_5.png')
         plt.show()
 
     else:
