b893cfbf9a6cf041102c001cf5482ebe16e9011f
Andrea Mattana <andrea.mattana@inaf.it>
2021-03-27 11:51:13 +0100

    Testing cplx

--- aavs_check_available_data.py
+++ aavs_check_available_data.py
@@ -165,9 +165,9 @@ if __name__ == "__main__":
         if opts.savecplx:
             for ant in range(16):
                 for npol, pol in enumerate(["Pol-X", "Pol-Y"]):
-                    fname = l[:-5] + "_INPUT-%02d_%s.txt" % (ant+1, pol)
-                    print l
-                    with open(fname, "w") as f:
+                    fname = l[:l.rfind('/')-1] + l[l.rfind('/') + 1:-5] + "_INPUT-%02d_%s.txt" % (ant + 1, pol)
+                    print fname
+                    with open(fname, "a") as f:
                         #for k in range(len(data[0, 0])/100):
                         #print ts_to_datestring(timestamps[0], formato="%Y-%m-%d %H:%M:%S.%s")
                         f.write("%f\t%s\t" % (timestamps[0], ts_to_datestring(timestamps[0])))
