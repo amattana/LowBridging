c276d391fdcd5e0c7efe1cb49f27af4e223edf37
Andrea Mattana <andrea.mattana@inaf.it>
2021-03-29 14:06:16 +0200

    Test mapping

--- aavs_check_available_data.py
+++ aavs_check_available_data.py
@@ -199,9 +199,10 @@ if __name__ == "__main__":
                             f.flush()
 
         if opts.saveraw and modo == FileDAQModes.Burst:
-            antenna_mapping = [0, 1, 2, 3, 8, 9, 10, 11, 15, 14, 13, 12, 7, 6, 5, 4]
+            #antenna_mapping = [0, 1, 2, 3, 8, 9, 10, 11, 15, 14, 13, 12, 7, 6, 5, 4]
             for tpm_input in opts.inputlist.split(","):
-                ant = antenna_mapping[int(tpm_input)]
+                #ant = antenna_mapping[int(tpm_input)]
+                ant = int(tpm_input)
                 for npol, pol in enumerate(["Pol-X", "Pol-Y"]):
                     fname = opts.outpath + "TILE-%02d_INPUT-%02d_%s_%s.raw" % \
                             (opts.tile, ant + 1, pol, ts_to_datestring(timestamps[0][0], formato="%Y-%m-%d_%H%M%S"))
